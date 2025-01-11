d75d0e69da3b93f1f48fc06097164dde6bcdd5d7
d4ff33 <193672396+d4ff33@users.noreply.github.com>
2025-01-10 23:57:30 -0500

    Remove about, add page loading functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* Logo Meme */
+
 const logofart1 = `../assets/logofart1.mp3`;
 const logofart2 = `../assets/logofart2.mp3`;
 const logofart3 = `../assets/logofart3.mp3`;
@@ -19,4 +21,33 @@ logofart.addEventListener("click", () => {
   setTimeout(() => {
     logofart.classList.remove("logo-shake");
   }, 700);
+});
+
+/*Page Loading*/
+
+const homeLink = document.getElementById("home_link");
+const galleryLink = document.getElementById("gallery_link");
+const doodlesLink = document.getElementById("doodles_link");
+const gamesLink = document.getElementById("games_link");
+
+const contentFrame = document.getElementById("iframe");
+
+homeLink.addEventListener("click", () => {
+  contentFrame.src = "pages/home.html";
+  document.title = "Daffy's Space - Home";
+});
+
+galleryLink.addEventListener("click", () => {
+  contentFrame.src = "pages/gallery.html";
+  document.title = "Daffy's Space - Gallery";
+});
+
+doodlesLink.addEventListener("click", () => {
+  contentFrame.src = "pages/doodles.html";
+  document.title = "Daffy's Space - Doodles";
+});
+
+gamesLink.addEventListener("click", () => {
+  contentFrame.src = "pages/games.html";
+  document.title = "Daffy's Space - Games";
 });
